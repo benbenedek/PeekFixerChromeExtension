a24c12a07fa1e79e59df2eb7801711a7c28102da
Asaf Ratzon <asaf.ratzon@gmail.com>
2018-11-25 19:14:24 -0500

    FIX: replacing tabs withs spaces

--- content.js
+++ content.js
@@ -1,43 +1,43 @@
 setTimeout(function onLoad() {
     if (typeof $ != 'undefined') {
 
-    	console.log("Peek Fixer Monitor")
+      console.log("Peek Fixer Monitor")
 
-        // ----======= Updating Dashboard Page =======----
+      // ----======= Updating Dashboard Page =======----
 
-    	// 	Clicking 'View all activities' link
-        if ($(".js-link:contains('View all activities')").length > 0) {
-            $(".js-link:contains('View all activities')")[0].click();
-        }
+      // Clicking 'View all activities' link
+      if ($(".js-link:contains('View all activities')").length > 0) {
+          $(".js-link:contains('View all activities')")[0].click();
+      }
 
-        // Hiding 'View activities with bookings' and 'View all activities' link
-        $(".js-link:contains('View all activities')").hide();
-        $(".js-link:contains('View activities with bookings')").hide();
+      // Hiding 'View activities with bookings' and 'View all activities' link
+      $(".js-link:contains('View all activities')").hide();
+      $(".js-link:contains('View activities with bookings')").hide();
 
 
-      	// ----======= Booking Options in Booking Wizard =======----
+      // ----======= Booking Options in Booking Wizard =======----
 
-      	// Clicking 'Customer Questions' link
-      	if ($(".js-link:contains('Customer Questions')").length > 0) {
-            $(".js-link:contains('Customer Questions')")[0].click();
-        }
-        $(".js-link:contains('Customer Questions')").hide();
+      // Clicking 'Customer Questions' link
+      if ($(".js-link:contains('Customer Questions')").length > 0) {
+          $(".js-link:contains('Customer Questions')")[0].click();
+      }
+      $(".js-link:contains('Customer Questions')").hide();
 
-        // Hiding 'Edit Price' link
-    	$(".js-link:contains('Edit Price')").hide();
+      // Hiding 'Edit Price' link
+      $(".js-link:contains('Edit Price')").hide();
 
-    	// Hiding 'Pay Later', 'Voucher' & 'Other' payent options
-    	$(".button-group-item:contains('Pay Later')").hide();
-    	$(".button-group-item:contains('Voucher')").hide();
-      	$(".button-group-item:contains('Other')").hide();
+      // Hiding 'Pay Later', 'Voucher' & 'Other' payent options
+      $(".button-group-item:contains('Pay Later')").hide();
+      $(".button-group-item:contains('Voucher')").hide();
+      $(".button-group-item:contains('Other')").hide();
 
-        // Block past dates - TODO
-        //var date = new Date(); // get local date
-        //var today = date.getDate();
-        //for (i = 1; i < today; ++i) {
-            //$(".:contains('" + i + "')").hide(); // remove link
-        //}
-    }
+      // Block past dates - TODO
+      //var date = new Date(); // get local date
+      //var today = date.getDate();
+      //for (i = 1; i < today; ++i) {
+          //$(".:contains('" + i + "')").hide(); // remove link
+      //}
+  }
 
-    setTimeout(onLoad, 1000);
+  setTimeout(onLoad, 1000);
 }, 1000);
